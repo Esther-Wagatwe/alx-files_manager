74e15a3145bb54d186f59f48a9168263afe505a4
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-10-14 23:31:46 +0300

    First file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -87,6 +87,74 @@ class FilesController {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
+
+  static async getShow(req, res) {
+    try {
+      const user = await UserUtils.getUserIdFromToken(req);
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const fileId = req.params.id || '';
+      const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: user._id });
+      if (!file) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      return res.status(200).json({
+        id: file._id,
+        userId: file.userId,
+        name: file.name,
+        type: file.type,
+        isPublic: file.isPublic,
+        parentId: file.parentId,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getIndex(req, res) {
+    try {
+      const user = await UserUtils.getUserIdFromToken(req);
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const parentId = req.query.parentId || '0';
+      const pagination = req.query.page || 0;
+
+      const aggregationMatch = { $and: [{ parentId }] };
+      let aggregateData = [
+        { $match: aggregationMatch },
+        { $skip: pagination * 20 },
+        { $limit: 20 },
+      ];
+      if (parentId === 0) aggregateData = [{ $skip: pagination * 20 }, { $limit: 20 }];
+
+      const files = await dbClient.db
+        .collection('files')
+        .aggregate(aggregateData);
+      const filesArray = [];
+      await files.forEach((item) => {
+        const fileItem = {
+          id: item._id,
+          userId: item.userId,
+          name: item.name,
+          type: item.type,
+          isPublic: item.isPublic,
+          parentId: item.parentId,
+        };
+        filesArray.push(fileItem);
+      });
+
+      return res.send(filesArray);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
 }
 
 export default FilesController;
